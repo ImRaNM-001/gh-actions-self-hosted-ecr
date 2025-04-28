8bdbec6a9769ad589f803bd3365418c7fe402a9a
ImRaNM-001 <imu163@gmail.com>
2025-04-29 00:04:55 +0530

    fix: resolve pipeline errors, uv dependencies clash & docker file
    fix: remove uv.lock from .dockerignore
    
    fix: update uv install step in Dockerfile
    
    test: update uv step dockerfile
    
    test: update uv step dockerfile_2
    
    test: update uv step dockerfile_3
    
    test: update uv step dockerfile_4
    
    test: update uv step dockerfile_5
    
    test: update uv step dockerfile_6
    
    test: update uv step dockerfile_7
    
    test: update uv step dockerfile_8
    
    test: update uv step dockerfile_9
    
    test: update uv step dockerfile_10
    
    test: update uv step dockerfile_11
    
    test: update uv step dockerfile_12
    
    test: run after success pipeline
    
    test: run after success pipeline_2
    
    chore: update final before pr merge

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,14 @@
 from calculator import add, subtract, multiply, divide
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 13)['result'] == 15
 
 def test_subtract():
-    assert subtract(5, 2) == 3
+    assert subtract(5, 18)['result'] == -13
 
 def test_multiply():
-    assert multiply(3, 4) == 12
+    assert multiply(3, 4)['result'] == 12
 
 def test_divide():
-    assert divide(10, 2) == 5
-    assert divide(10, 0) == 'Error: Division by zero'
+    assert divide(90, 2)['result'] == 45.0
+    assert divide(10, 0)['result'] == 'Error: Division by zero not feasible'
